f1727e2bbd07d6e1fcf859e87b7ca420f27d438a
allexkap <aekpstn@gmail.com>
2023-04-12 15:12:38 +0300

    change timer two to one

--- prealarm.ino
+++ prealarm.ino
@@ -1,34 +1,37 @@
-volatile uint8_t value = 150;
+uint16_t delta = 18000;
 
 
 void setup() {
+    Serial.begin(115200);
     light_dimmer();
 }
 
 void loop() {
-    if (!--value) value -= 110;
-    delay(100);
+    while (!Serial.available());
+    if (Serial.read() == '.') delta -= 256;
+    else delta += 256;
+    Serial.println(delta);
 }
 
 
 void light_dimmer() {
     cli();
     DDRD |= (1 << 6);
-    TCCR2A = 0b0;
-    TCCR2B = 0b111;
-    attachInterrupt(0, zero_cross, RISING);
+    TCCR1A = 0b00;      // обычный режим
+    TCCR1B = 0b010;     // прескелер на 8
+    OCR1A = 0;          // значение A
+    TIMSK1 = 0b010;     // разрешаем А
+    attachInterrupt(0, trigger, RISING);
     sei();
 }
 
-void zero_cross() {
+void trigger() {
     PORTD &= ~(1 << 6);
-    GTCCR |= 2;
-    TCNT2 = 0;
-    TIMSK2 = 0b10;
-    OCR2A = value;
+    OCR1A = delta;      // значение A
+    TIFR1 |= (1 << 1);  // сброс флага A
+    TCNT1 = 0;          // сброс счетчика
 }
 
-ISR(TIMER2_COMPA_vect) {
+ISR(TIMER1_COMPA_vect) {
     PORTD |= (1 << 6);
-    TIMSK2 = 0;
 }
